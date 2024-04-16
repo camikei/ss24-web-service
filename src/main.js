1859af6c91a6815995d25847c8843fce71e66b83
camikei <3121414@stud.srh-campus-berlin.de>
2024-04-16 11:46:15 +0200

    april 16 changes

--- src/main.js
+++ src/main.js
@@ -1,33 +1,36 @@
 const express = require('express')
 const app = express()
-app.use(express.urlencoded({extended:true}))
 
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/public/index.html')
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.sendFile(`${__dirname}/public/index.html`)
 })
-app.post('/create-avatar',function (req, res){
+app.post('/api/avatars',async (req, res) =>{
+    console.log("POST/api/avatars")
     console.log(req.body);
 
     const avatar = {
         id: Date.now(),
         avatarName: req.body.avatarName,
-        childAge: req.body.childAge,
+        childAge: parseInt(req.body.childAge),
         skinColor: req.body.skinColor,
         hairstyle: req.body.hairstyle,
         headShape: req.body.headShape,
         upperClothing: req.body.upperClothing,
+        lowerClothing: req.body.lowerClothing,
         createdAt: new Date().toISOString()
     }
 
     try {
-        const data = await fs.readFileSync(`${__dirname}/public/avatars.json`)
-        const currentAvatars = JSON.parse(data)
+        const data = await fs.readFileSync(`${__dirname}/public/avatars.json`);
+        const avatars = JSON.parse(data)
 
-        currentAvatars.push(avatar)
+        avatars.push(avatar)
 
-        await fs.writeFileSync(`${__dirname}/public/avatars.json`, JSON.stringify(currentAvatars))
+        await fs.writeFileSync(`${__dirname}/public/avatars.json`, JSON.stringify(avatars))
 
-        res.sendStatus(200)
+        res.sendStatus(200).set("Location", `/avatar/${avatar.id}`).send(avatar)
     } catch (error){
         res.sendStatus(500)
     }
